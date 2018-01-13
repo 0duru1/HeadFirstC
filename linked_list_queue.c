e32cb551178578e31fa4d244eae531de6c82e8ed
duru <duru@opentutorials.org>
2018-01-13 22:03:13 +0900

    링크드리스트를 이용한 큐 구현

---
+++ linked_list_queue.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef int element;
+
+typedef struct {
+	element item;
+	struct QueueNode *link;
+} QueueNode;
+
+typedef struct {
+	//front, rear
+	//queue
+	QueueNode *front, *rear;
+} Queue;
+
+void err(char *msg)
+{
+	fprintf(stderr, "%s \n", msg);
+	exit(1);
+}
+
+void init(Queue *q)
+{
+	q->front = q->rear = 0;
+}
+
+int is_empty(Queue *q)
+{
+	return (q->front==NULL);
+}
+
+int is_full(Queue *q)
+{
+	return 0;
+}
+
+void enqueue(Queue *q, element item)
+{
+/*
+	//동적메모리를 할당을 통해 새로운 노드를 만든다.
+	//새로운 노드에 데이타를 저장한다.
+	//큐의 끝노드의 링크를 새로운 노드를 가리키게 한다.
+	//큐의 마지막포인트를 새로운 노드를 가리키게 한다.
+
+	QueueNode *tmp = (QueueNode *)malloc(sizeof(QueueNode));
+	if(tmp == NULL){
+		err("not allocation memory");
+	} else {
+		tmp->item = item;
+		tmp->link = NULL;
+		if(is_empty(q)) {
+			q->front = tmp;
+			q->rear = tmp;
+		} else {
+			q->rear->link = tmp;
+			q->rear = tmp;
+		}
+	}
+*/
+}
+
+void  dequeue(Queue *q)
+{
+	//먼저 큐가 공백인지 검사
+	//공백이면->에러표시
+	//공백이 아니면
+		//프론트가 가리키는 노드를 임시 노드가 가리키도록 한다
+		//프론터가 가리키는 노드의 링크를 프롵너가 가리키도록 한다.
+		//템프노드가 가리키는 곳을 메모리에서 해제한다.
+}
+
+int main()
+{
+	printf("queue create\n");
+	Queue q;
+	init(&q);
+
+	return 0;
+}
