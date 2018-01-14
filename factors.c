b3bc506af705b752f23a7a79e53f06cd28123f8f
duru <duru@opentutorials.org>
2018-01-15 01:57:33 +0900

    약수를 구하기

---
+++ factors.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+	int factor = 1800;
+	int cnt = 0;
+	printf("%d의  약수의 갯수를 구하여라\n", factor);
+
+	clock_t start, finish;
+	double duration;
+
+	//알고리즘 수행 시간 측정
+	start = clock();
+	for(int i = 1; i <= factor; i++)
+	{
+		if(factor%i == 0)
+		 	cnt=cnt+1;
+	}
+	finish = clock();
+	//-----------------------
+
+	duration = (double)(finish - start) / CLOCKS_PER_SEC;
+	printf("%d의 약수의 갯수는 : %d\n", factor, cnt);
+	printf("%f의 초 걸림 \n", duration);
+
+	
+	return 0;
+}
